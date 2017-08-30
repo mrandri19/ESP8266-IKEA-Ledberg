2685e5fd20a08c71da7bc629d7ee67f5d80a6174
mrandri19 <andrecogno@hotmail.it>
2017-08-31 00:18:21 +0200

    First working version

--- frontend/index.js
+++ frontend/index.js
@@ -2,21 +2,34 @@ $(document).ready(() => {
     const esp_ip = "192.168.10.109"
     const ws = new WebSocket(`ws://${esp_ip}/rpc/`)
     console.log("creating ws")
-    const rpc_frame = {
-        method: "LED.ChangeValue"
-    }
-
-    ws.onopen = evt => {
-        console.log("ws opened", evt)
-        ws.send(JSON.stringify(rpc_frame))
-    }
-
-    ws.onmessage = evt => console.log("ws got a message", JSON.parse(evt.data).result.hello)
 
     ws.onclose = evt => console.log("ws closed", evt)
+    ws.onopen = evt => console.log("ws opened", evt)
 
+    ws.onmessage = evt => {
+        let data = JSON.parse(evt.data)
+        console.log("ws got a message", data)
+        if ("error" in data) {
+            console.log("Error:", data.error.code, data.error.message)
+            $("#result").text("Error");
+        }
+        if ("result" in data) {
+            console.log("Success new value:", data.result.msg)
+            $("#result").text("Success new value:" + data.result.msg);
+        }
+    }
 
     const slider = $('#ex1').slider().data('slider')
-    slider.on('slideStop', () => console.log(`value changed: ${slider.getValue()}`))
+    slider.on('slideStop', () => {
+        console.log(`value changed: ${slider.getValue()}`)
+        const rpc_frame = {
+            method: "LED.ChangeValue",
+            args: {
+                new_value: slider.getValue()
+            }
+        }
+        ws.send(JSON.stringify(rpc_frame))
+        console.log("sent ws message")
+    })
 
 })
--- src/main.c
+++ src/main.c
@@ -1,17 +1,29 @@
 #include "mgos.h"
 #include "mgos_rpc.h"
+#include "mgos_pwm.h"
+
 
 void rpc_hello_world_handler(struct mg_rpc_request_info *ri, void *cb_arg,
                              struct mg_rpc_frame_info *fi,
                              struct mg_str args) {
     int new_value = 0;
     bool res = json_scanf(args.p, (int) args.len, ri->args_fmt, &new_value) <= 0;
-    printf("%d\n", res);
     if (res) {
-        printf("sending error\n");
         mg_rpc_send_errorf(ri, -1, "invalid json: %.*s", args.len, args.p);
         return;
     }
+    if (new_value < 0 || new_value > 20) {
+        mg_rpc_send_errorf(ri, -2, "invalid new_value: %d. Must be between [0-20]", new_value);
+        return;
+    }
+    const int LED_PIN = 2;
+    const int FREQ = 1E3;
+
+    if (!mgos_pwm_set(LED_PIN, FREQ, (100 - (new_value * 5)))) {
+        mg_rpc_send_errorf(ri, -3, "error setting led's pwm");
+        return;
+    }
+
     mg_rpc_send_responsef(ri, "{msg: %d}", new_value);
     (void) cb_arg;
     (void) fi;
